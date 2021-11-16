eac5d7b17dae38fcc2352ec0b8db5b8c962db3ac
Jimmyjamjam13 <james13ryan@gmail.com>
2021-11-16 14:37:18 +0000

    add board class and define with placeholder of action of game

--- run.py
+++ run.py
@@ -1,3 +1,54 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+from random import randint
+
+scores ={"computer": 0, "player": 0}
+
+
+class board:
+
+
+
+
+    def __init__(self, size, num_ships, names, type):
+        self.size = size
+        self.board =[["." for x in range(size)] for y in range(size)]
+        self.num_ships =num_ships
+        self.name = names
+        self.type = type
+        self.guesses = []
+        self.ships = []
+
+    def print_board(self):
+        for row in self.board:
+            print(' '.join(row))
+
+    def board_guess(self, x, y):
+        self.guesses.append((x, y))
+        self.board[x][y] = 'X'
+
+        if (x, y) in self.ships:
+            self.board[x][y] = '| O'
+            return f'{self.name}, you hit and sank a battleship!'
+        else:
+            return f"{self.name}, you've missed this time..."
+
+    def add_ships(self, x, y, type='computer'):
+        if len(self.ships) >= self.ship_nums:
+            print('Too many ships')
+
+        else:
+            self.ships.append((x, y))
+            if self.type == 'player':
+                self.board[x][y] = '| @'
+
+
+def random_number(size):
+    """
+    Returns random integer between 0 and the length of the board
+    chosen by the player.
+    """
+    return randint(0, (size) - 1)
+        
+
+
+
+
